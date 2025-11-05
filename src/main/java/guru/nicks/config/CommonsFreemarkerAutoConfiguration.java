f695e1fbbe3572b82c6ff47ce4b55934c490d52a
Nick S. <job-offers@nicks.guru>
2025-11-05 14:44:16 +0300

    Bumped version number

--- src/main/java/guru/nicks/config/CommonsFreemarkerAutoConfiguration.java
+++ src/main/java/guru/nicks/config/CommonsFreemarkerAutoConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-public class FreemarkerAutoConfiguration {
+public class CommonsFreemarkerAutoConfiguration {
 
     @ConditionalOnMissingBean(FreemarkerTemplateService.class)
     @Bean

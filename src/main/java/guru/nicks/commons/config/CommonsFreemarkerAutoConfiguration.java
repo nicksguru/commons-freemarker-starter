fb89083f1975f484a2e2d2a9a8568b440dfca139
Nick S. <job-offers@nicks.guru>
2025-11-19 15:22:34 +0300

    Bumped version number

--- src/main/java/guru/nicks/commons/config/CommonsFreemarkerAutoConfiguration.java
+++ src/main/java/guru/nicks/commons/config/CommonsFreemarkerAutoConfiguration.java
@@ -3,16 +3,22 @@ package guru.nicks.commons.config;
 import guru.nicks.commons.impl.FreemarkerTemplateServiceImpl;
 import guru.nicks.commons.service.FreemarkerTemplateService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
+@Slf4j
 public class CommonsFreemarkerAutoConfiguration {
 
+    /**
+     * Creates {@link FreemarkerTemplateService} bean if it's not already present.
+     */
     @ConditionalOnMissingBean(FreemarkerTemplateService.class)
     @Bean
     public FreemarkerTemplateService freemarkerTemplateService(freemarker.template.Configuration configuration) {
+        log.debug("Building {} bean", FreemarkerTemplateService.class.getSimpleName());
         return new FreemarkerTemplateServiceImpl(configuration);
     }
 
